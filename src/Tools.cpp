dfb0515b52ef84d5befb0b1daa03e9cac73d1cef
FXDuke <113947184+FXDuke@users.noreply.github.com>
2024-03-07 14:14:11 -0500

    1.0.0

---
+++ src/Tools.cpp
@@ -0,0 +1,154 @@
+#include "Tools.h"
+using namespace Tools;
+
+void LED::_updateTween()
+{
+    f_delta = min(f_delta + CLOCK_TIME, f_time);
+
+    i32_brightness = i32_brightnessOrigin + i32_brightnessDelta * f_delta;
+
+    tweening = (f_delta < f_time);
+
+    if (!tweening)
+    {
+        i32_brightness = i32_brightnessGoal;
+    }
+}
+
+int LED::GetBrightness()
+{
+    return i32_brightness;
+}
+
+bool LED::Mode()
+{
+    return b_pinType;
+}
+
+void LED::EnableDigital()
+{
+    b_pinType = 1;
+}
+
+void LED::EnableAnalog()
+{
+    b_pinType = 0;
+}
+
+void LED::Brightness(int power)
+{
+    i32_brightness = (power > 255) ? 255 : (power < 0) ? 0
+                                                       : power;
+}
+
+void LED::SetPin(int pin)
+{
+    pinMode(pin, OUTPUT);
+    i32_pin = pin;
+}
+
+void LED::Tween(int power, float time)
+{
+    i32_brightnessOrigin = i32_brightness;
+    i32_brightnessGoal = power;
+    i32_brightnessDelta = (power - i32_brightness) / time;
+    f_time = time;
+    f_delta = 0.0;
+
+    tweening = true;
+}
+
+void LED::BlinkOn(int delay)
+{
+    i32_blinkTime = max(CLOCK_TIME, delay);
+    blink = 1;
+}
+
+void LED::BlinkOff()
+{
+    blink = 0;
+}
+
+void LED::On()
+{
+    b_on = 1;
+}
+
+void LED::Off()
+{
+    b_on = 0;
+}
+
+void LED::Update()
+{
+
+    bool on = b_on;
+
+    if (tweening)
+    {
+        _updateTween();
+    }
+
+    if (blink)
+    {
+
+        if (i32_blinkCooldown > 0)
+        {
+            i32_blinkCooldown -= CLOCK_TIME_MS;
+            return;
+        }
+
+        i32_blinkCooldown = i32_blinkTime;
+        b_blinkOn = !b_blinkOn;
+        on = b_blinkOn & on;
+    }
+
+    switch (b_pinType)
+    {
+    case 0:
+        analogWrite(i32_pin, i32_brightness * on);
+        break;
+    case 1:
+        digitalWrite(i32_pin, HIGH * on + LOW * (!on));
+        break;
+    }
+}
+
+void Button::Update()
+{
+
+    bool read = digitalRead(i32_pin);
+
+    switch (read)
+    {
+    case 0:
+        held_time += CLOCK_TIME;
+        if (!b_debounce)
+        {
+            pressed = 1;
+            held = 1;
+            b_debounce = 1;
+            break;
+        }
+        pressed = 0;
+        break;
+    case 1:
+        held_time = 0.0;
+        if (b_debounce)
+        {
+            released = 1;
+            b_debounce = 0;
+            held = 0;
+            pressed = 0;
+            break;
+        }
+        released = 0;
+        break;
+    }
+}
+
+Button::Button(int pin)
+{
+    i32_pin = pin;
+    pinMode(pin, INPUT_PULLUP);
+}
---
+++ src/Tools.h
@@ -0,0 +1,83 @@
+#pragma once
+
+namespace Tools {
+
+    float CLOCK_TIME = 0.01;
+    int CLOCK_TIME_MS = CLOCK_TIME*1000;
+
+    void ChangeClock(float rate)
+    {
+        CLOCK_TIME = rate;
+        CLOCK_TIME_MS = rate*1000;
+    };
+
+    class LED {
+
+    private:
+
+        // Basic
+        int i32_pin = 0;
+
+        int i32_brightness = 100;   // Only active with analog output
+
+        bool b_pinType = 1;         // 1 = Digital, 0 = Analog
+
+        bool b_on = 0;
+
+        int i32_blinkTime = CLOCK_TIME_MS;
+        int i32_blinkCooldown = 0;
+        bool b_blinkOn = 0;        // Flips on and off on a delay, doenst set b_on;
+
+        // Tween
+
+        int i32_brightnessDelta;
+        int i32_brightnessGoal;
+        int i32_brightnessOrigin = 100;
+        float f_delta = 0.0;
+        float f_time = 0.0;
+
+        void _updateTween();
+
+    public:
+
+        bool tweening = 0;
+        bool blink = 0;
+
+        int GetBrightness();
+        bool Mode();
+        void EnableDigital();
+        void EnableAnalog();
+        void Brightness(int);
+        void SetPin(int);
+        void Tween(int, float);
+        void BlinkOn(int);
+        void BlinkOff();
+        void On();
+        void Off();
+        void Update();
+
+    };
+
+    class Button
+    {
+
+    private:
+
+        int i32_pin = 0;
+        bool b_debounce = 0;
+
+    public:
+
+        bool pressed = 0;
+        bool released = 0;
+        bool held = 0;
+        
+        float held_time = 0.0;
+
+        void Update();
+
+        Button(int);
+
+    };
+
+};
